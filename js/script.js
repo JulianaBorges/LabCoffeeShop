eca1c1b5aa22eb482f6248c2bf6602fe17f995f2
Juliana Borges <juuh.boges@hotmail.com>
2022-08-04 15:15:54 -0300

    adiça da seçao facility

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .flex .navbar');
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
     
 }
 window.onscroll = () =>{
